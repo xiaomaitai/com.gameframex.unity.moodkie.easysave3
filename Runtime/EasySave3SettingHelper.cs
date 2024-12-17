4e90e75f9056a0565a017466c34dcc3bf09d0150
Blank <wangfj11@foxmail.com>
2024-12-17 20:53:45 +0800

    [增加]1. 增加存档适配器

---
+++ Runtime/EasySave3SettingHelper.cs
@@ -0,0 +1,353 @@
+﻿#if ENABLE_GAME_FRAME_X && ENABLE_GAME_FRAME_X_SETTING
+using System;
+using System.Collections.Generic;
+using System.IO;
+using GameFrameX.Runtime;
+using UnityEngine;
+
+namespace GameFrameX.Setting.Runtime
+{
+    /// <summary>
+    /// EasySave3游戏配置辅助器。
+    /// </summary>
+    [UnityEngine.Scripting.Preserve]
+    public class EasySave3SettingHelper : SettingHelperBase
+    {
+        private const string SettingFileName = "GameFrameXSetting.sav";
+        private string m_FilePath = null;
+
+        /// <summary>
+        /// 获取游戏配置项数量。
+        /// </summary>
+        public override int Count
+        {
+            get { return -1; }
+        }
+
+        /// <summary>
+        /// 获取游戏配置存储文件路径。
+        /// </summary>
+        public string FilePath
+        {
+            get { return m_FilePath; }
+        }
+
+        /// <summary>
+        /// 加载游戏配置。
+        /// </summary>
+        /// <returns>是否加载游戏配置成功。</returns>
+        public override bool Load()
+        {
+            try
+            {
+                if (!File.Exists(m_FilePath))
+                {
+                    return true;
+                }
+
+                ES3.Init();
+                return true;
+            }
+            catch (Exception exception)
+            {
+                Log.Warning("Load settings failure with exception '{0}'.", exception);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 保存游戏配置。
+        /// </summary>
+        /// <returns>是否保存游戏配置成功。</returns>
+        public override bool Save()
+        {
+            if (File.Exists(m_FilePath))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// 获取所有游戏配置项的名称。
+        /// </summary>
+        /// <returns>所有游戏配置项的名称。</returns>
+        public override string[] GetAllSettingNames()
+        {
+            return ES3.GetKeys(_settings);
+        }
+
+        /// <summary>
+        /// 获取所有游戏配置项的名称。
+        /// </summary>
+        /// <param name="results">所有游戏配置项的名称。</param>
+        public override void GetAllSettingNames(List<string> results)
+        {
+            var values = ES3.GetKeys(_settings);
+            if (results == null)
+            {
+                results = new List<string>(values.Length);
+            }
+
+            results.AddRange(values);
+        }
+
+        /// <summary>
+        /// 检查是否存在指定游戏配置项。
+        /// </summary>
+        /// <param name="settingName">要检查游戏配置项的名称。</param>
+        /// <returns>指定的游戏配置项是否存在。</returns>
+        public override bool HasSetting(string settingName)
+        {
+            return ES3.KeyExists(settingName, _settings);
+        }
+
+        /// <summary>
+        /// 移除指定游戏配置项。
+        /// </summary>
+        /// <param name="settingName">要移除游戏配置项的名称。</param>
+        /// <returns>是否移除指定游戏配置项成功。</returns>
+        public override bool RemoveSetting(string settingName)
+        {
+            try
+            {
+                ES3.DeleteKey(settingName, _settings);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 清空所有游戏配置项。
+        /// </summary>
+        public override void RemoveAllSettings()
+        {
+            ES3.DeleteFile(_settings);
+        }
+
+        /// <summary>
+        /// 从指定游戏配置项中读取布尔值。
+        /// </summary>
+        /// <param name="settingName">要获取游戏配置项的名称。</param>
+        /// <returns>读取的布尔值。</returns>
+        public override bool GetBool(string settingName)
+        {
+            return ES3.Load<bool>(settingName, _settings);
+        }
+
+        /// <summary>
+        /// 从指定游戏配置项中读取布尔值。
+        /// </summary>
+        /// <param name="settingName">要获取游戏配置项的名称。</param>
+        /// <param name="defaultValue">当指定的游戏配置项不存在时，返回此默认值。</param>
+        /// <returns>读取的布尔值。</returns>
+        public override bool GetBool(string settingName, bool defaultValue)
+        {
+            return ES3.Load<bool>(settingName, defaultValue, _settings);
+        }
+
+        /// <summary>
+        /// 向指定游戏配置项写入布尔值。
+        /// </summary>
+        /// <param name="settingName">要写入游戏配置项的名称。</param>
+        /// <param name="value">要写入的布尔值。</param>
+        public override void SetBool(string settingName, bool value)
+        {
+            ES3.Save(settingName, value, _settings);
+        }
+
+        /// <summary>
+        /// 从指定游戏配置项中读取整数值。
+        /// </summary>
+        /// <param name="settingName">要获取游戏配置项的名称。</param>
+        /// <returns>读取的整数值。</returns>
+        public override int GetInt(string settingName)
+        {
+            return ES3.Load<int>(settingName, 0, _settings);
+        }
+
+        /// <summary>
+        /// 从指定游戏配置项中读取整数值。
+        /// </summary>
+        /// <param name="settingName">要获取游戏配置项的名称。</param>
+        /// <param name="defaultValue">当指定的游戏配置项不存在时，返回此默认值。</param>
+        /// <returns>读取的整数值。</returns>
+        public override int GetInt(string settingName, int defaultValue)
+        {
+            return ES3.Load<int>(settingName, defaultValue, _settings);
+        }
+
+        /// <summary>
+        /// 向指定游戏配置项写入整数值。
+        /// </summary>
+        /// <param name="settingName">要写入游戏配置项的名称。</param>
+        /// <param name="value">要写入的整数值。</param>
+        public override void SetInt(string settingName, int value)
+        {
+            ES3.Save(settingName, value, _settings);
+        }
+
+        /// <summary>
+        /// 从指定游戏配置项中读取浮点数值。
+        /// </summary>
+        /// <param name="settingName">要获取游戏配置项的名称。</param>
+        /// <returns>读取的浮点数值。</returns>
+        public override float GetFloat(string settingName)
+        {
+            return ES3.Load<float>(settingName, _settings);
+        }
+
+        /// <summary>
+        /// 从指定游戏配置项中读取浮点数值。
+        /// </summary>
+        /// <param name="settingName">要获取游戏配置项的名称。</param>
+        /// <param name="defaultValue">当指定的游戏配置项不存在时，返回此默认值。</param>
+        /// <returns>读取的浮点数值。</returns>
+        public override float GetFloat(string settingName, float defaultValue)
+        {
+            return ES3.Load<float>(settingName, defaultValue, _settings);
+        }
+
+        /// <summary>
+        /// 向指定游戏配置项写入浮点数值。
+        /// </summary>
+        /// <param name="settingName">要写入游戏配置项的名称。</param>
+        /// <param name="value">要写入的浮点数值。</param>
+        public override void SetFloat(string settingName, float value)
+        {
+            ES3.Save(settingName, value, _settings);
+        }
+
+        /// <summary>
+        /// 从指定游戏配置项中读取字符串值。
+        /// </summary>
+        /// <param name="settingName">要获取游戏配置项的名称。</param>
+        /// <returns>读取的字符串值。</returns>
+        public override string GetString(string settingName)
+        {
+            return ES3.LoadString(settingName, null, _settings);
+        }
+
+        /// <summary>
+        /// 从指定游戏配置项中读取字符串值。
+        /// </summary>
+        /// <param name="settingName">要获取游戏配置项的名称。</param>
+        /// <param name="defaultValue">当指定的游戏配置项不存在时，返回此默认值。</param>
+        /// <returns>读取的字符串值。</returns>
+        public override string GetString(string settingName, string defaultValue)
+        {
+            return ES3.LoadString(settingName, defaultValue, _settings);
+        }
+
+        /// <summary>
+        /// 向指定游戏配置项写入字符串值。
+        /// </summary>
+        /// <param name="settingName">要写入游戏配置项的名称。</param>
+        /// <param name="value">要写入的字符串值。</param>
+        public override void SetString(string settingName, string value)
+        {
+            ES3.Save(settingName, value, _settings);
+        }
+
+        /// <summary>
+        /// 从指定游戏配置项中读取对象。
+        /// </summary>
+        /// <typeparam name="T">要读取对象的类型。</typeparam>
+        /// <param name="settingName">要获取游戏配置项的名称。</param>
+        /// <returns>读取的对象。</returns>
+        public override T GetObject<T>(string settingName)
+        {
+            var value = GetString(settingName);
+            if (value.IsNullOrWhiteSpace())
+            {
+                return new T();
+            }
+
+            return Utility.Json.ToObject<T>(value);
+        }
+
+        /// <summary>
+        /// 从指定游戏配置项中读取对象。
+        /// </summary>
+        /// <param name="objectType">要读取对象的类型。</param>
+        /// <param name="settingName">要获取游戏配置项的名称。</param>
+        /// <returns>读取的对象。</returns>
+        public override object GetObject(Type objectType, string settingName)
+        {
+            return Utility.Json.ToObject(objectType, GetString(settingName));
+        }
+
+        /// <summary>
+        /// 从指定游戏配置项中读取对象。
+        /// </summary>
+        /// <typeparam name="T">要读取对象的类型。</typeparam>
+        /// <param name="settingName">要获取游戏配置项的名称。</param>
+        /// <param name="defaultObj">当指定的游戏配置项不存在时，返回此默认对象。</param>
+        /// <returns>读取的对象。</returns>
+        public override T GetObject<T>(string settingName, T defaultObj)
+        {
+            string json = GetString(settingName, null);
+            if (json.IsNullOrWhiteSpace())
+            {
+                return defaultObj;
+            }
+
+            return Utility.Json.ToObject<T>(json);
+        }
+
+        /// <summary>
+        /// 从指定游戏配置项中读取对象。
+        /// </summary>
+        /// <param name="objectType">要读取对象的类型。</param>
+        /// <param name="settingName">要获取游戏配置项的名称。</param>
+        /// <param name="defaultObj">当指定的游戏配置项不存在时，返回此默认对象。</param>
+        /// <returns>读取的对象。</returns>
+        public override object GetObject(Type objectType, string settingName, object defaultObj)
+        {
+            string json = GetString(settingName, null);
+            if (json.IsNullOrWhiteSpace())
+            {
+                return defaultObj;
+            }
+
+            return Utility.Json.ToObject(objectType, json);
+        }
+
+        /// <summary>
+        /// 向指定游戏配置项写入对象。
+        /// </summary>
+        /// <typeparam name="T">要写入对象的类型。</typeparam>
+        /// <param name="settingName">要写入游戏配置项的名称。</param>
+        /// <param name="obj">要写入的对象。</param>
+        public override void SetObject<T>(string settingName, T obj)
+        {
+            SetString(settingName, Utility.Json.ToJson(obj));
+        }
+
+        /// <summary>
+        /// 向指定游戏配置项写入对象。
+        /// </summary>
+        /// <param name="settingName">要写入游戏配置项的名称。</param>
+        /// <param name="obj">要写入的对象。</param>
+        public override void SetObject(string settingName, object obj)
+        {
+            SetString(settingName, Utility.Json.ToJson(obj));
+        }
+
+        private ES3Settings _settings;
+
+        private void Awake()
+        {
+            m_FilePath = Utility.Path.GetRegularPath(Path.Combine(Application.persistentDataPath, SettingFileName));
+            _settings = ES3Settings.defaultSettings;
+            _settings.path = m_FilePath;
+            _settings.encryptionType = ES3.EncryptionType.AES;
+            _settings.encryptionPassword = "com.alianblank.gameframex.unity";
+        }
+    }
+}
+#endif
